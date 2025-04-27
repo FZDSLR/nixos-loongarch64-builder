66dec6c8bdb69dfd2f004c47349bad62fde27fe0
fzdslr-nw <fzdslr@qq.com>
2025-04-27 06:14:50 +0000

    fix: switch to nix-store --realise

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ nix eval --raw ".#nixosConfigurations.loongarch64.config.environment.systemPacka
   --apply "packages: builtins.concatStringsSep \"\n\" (map (pkg: pkg.drvPath) packages)" \
   | grep -E '^/nix/store/[0-9a-df-np-tv-z]{32}-[^/]+\.drv$' \
   | sort -u \
-  | xargs -I{} nix build --print-build-logs {}
+  | xargs -I{} nix-store --realise --verbose {}
