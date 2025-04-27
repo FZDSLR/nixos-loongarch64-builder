d2fca9c6b4d3f56182a93d36b387df890f1cf746
fzdslr-nw <fzdslr@qq.com>
2025-04-27 11:15:21 +0000

    add: cachix

--- build.sh
+++ build.sh
@@ -10,14 +10,14 @@ nix eval --raw ".#nixosConfigurations.loongarch64.config.environment.systemPacka
   | sort -u \
   | xargs -I{} sh -c '
       drv_path="$1"
-      pkg_name=$(basename "$drv_path" | sed -E "s/^[^-]*-([^.]*)\.drv$/\1/")
+      pkg_name=$(basename "$drv_path")
       echo "开始构建 $pkg_name"
 
       log_file=$(mktemp)
       if nix-store --realise --verbose "$drv_path" > "$log_file" 2>&1; then
-        # result_path=$(nix-store --query --binding out "$drv_path")
-        # echo "上传 $pkg_name 到Cachix"
-        # cachix push my-cache "$result_path"
+        result_path=$(nix-store --query --binding out "$drv_path")
+        echo "上传 $pkg_name 到Cachix"
+        cachix push loongarch64-cross-test "$result_path"
         rm -f "$log_file"
       else
         echo "$drv_path" >> "$2"
