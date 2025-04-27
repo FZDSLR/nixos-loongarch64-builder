167d48703ac533ef1d8f94d5b6fe3c0cd86a5b52
fzdslr-nw <fzdslr@qq.com>
2025-04-27 14:04:29 +0000

    fix: libressl source sha256

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,7 +5,7 @@ self: super: {
         src = super.fetchgit {
           url = "https://github.com/libressl/portable.git";
           rev = "031c2f1722f9af10299de3d22ff3c1467d541241";
-          sha256 = "sha256-1rj2v0ch0iscr0vsg4xmck71bgfxk7vkwa4zxf8fqjxwjc949qw9";
+          sha256 = "sha256-ieNEEpO8S+yQ658oPveZ3b0VzmS1k6c3yExHABnYQuY=";
         };
       }
     )
