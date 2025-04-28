51398802e23356ad5f3320957b451428616ee369
fzdslr-nw <fzdslr@qq.com>
2025-04-28 10:21:24 +0000

    fix: remove openblas BINARY=64 flag

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,11 +11,16 @@
       libressl = super.libressl_3_6;
 
       webrtc-audio-processing = super.webrtc-audio-processing.overrideAttrs (oldAttrs: {
-        meta.platforms = oldAttrs.meta.platforms ++ ["loongarch64-linux"];
+        meta.platforms = oldAttrs.meta.platforms ++ [ "loongarch64-linux" ];
       });
       webrtc-audio-processing_1 = super.webrtc-audio-processing_1.overrideAttrs (oldAttrs: {
-        meta.platforms = oldAttrs.meta.platforms ++ ["loongarch64-linux"];
+        meta.platforms = oldAttrs.meta.platforms ++ [ "loongarch64-linux" ];
       });
+
+      openblas = super.openblas.overrideAttrs (old: {
+        makeFlags = super.lib.lists.remove "BINARY=64" old.makeFlags;
+      });
+
     })
   ];
 }
