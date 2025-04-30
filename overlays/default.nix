231525cebd9716f794910c0053856fe80a20a1ee
fzdslr-nw <fzdslr@qq.com>
2025-04-30 04:12:39 +0000

    fix: x264 remove as config

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,14 +13,6 @@
       openblas = super.openblas.overrideAttrs (old: {
         makeFlags = super.lib.lists.remove "BINARY=64" old.makeFlags;
       });
-
-      x264 = super.x264.overrideAttrs (old: {
-        preConfigure =
-          (old.preConfigure or "")
-          + lib.optionalString super.stdenv.hostPlatform.isLoongArch64 ''
-            export AS=$CC
-          '';
-      });
     })
   ];
 }
