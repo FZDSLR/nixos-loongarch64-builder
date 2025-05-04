8a2220c835a3316132e80fdedceccf9c50ede2c4
fzdslr-nw <fzdslr@qq.com>
2025-05-04 16:05:35 +0000

    update: remove overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,12 +7,6 @@
 
 {
   nixpkgs.overlays = [
-    (self: super: {
-      libressl = super.libressl_3_6;
 
-      openblas = super.openblas.overrideAttrs (old: {
-        makeFlags = super.lib.lists.remove "BINARY=64" old.makeFlags;
-      });
-    })
   ];
 }
