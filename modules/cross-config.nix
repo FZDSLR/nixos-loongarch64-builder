812132e68763469ca91c22dda7dbaf000506d60e
fzdslr-nw <fzdslr@qq.com>
2025-06-04 17:12:48 +0000

    test: crossPlatform.rust

--- modules/cross-config.nix
+++ modules/cross-config.nix
@@ -8,13 +8,13 @@
       name = "loong64";
       target = "uImage";
     };
-    rustc.config = "loongarch64-unknown-linux-gnu";
-    rustc.platform = {
+    rust.rustcTarget = "loongarch64-unknown-linux-gnu";
+    rust.platform = {
       arch = "loongarch64";
       os = "linux";
       target-family = [ "unix" ];
       vendor = "unknown";
-      features= "+f,+d";
+      features= "+f,+d,-lsx";
     };
   };
   nixpkgs.overlays = [(import ../overlays/default.nix)];
