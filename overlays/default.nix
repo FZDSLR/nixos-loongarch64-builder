c0167b4f36088c3c88e5546160dde71cea7b1594
fzdslr-nw <fzdslr@qq.com>
2025-06-08 04:43:46 +0000

    fix: overlay rust when cross and add experimental uboot support

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,14 +2,8 @@
   self: super:
 
   let
-    rustPackages = [
-      "nsncd"
-      "eza"
-      "bat"
-      "netavark"
-      "aardvark-dns"
-    ];
-    updateRustPlatform = pkg: pkg.override { rustPlatform = self.rustPlatform_1_83; };
+    isCross = super.stdenv.buildPlatform != super.stdenv.hostPlatform;
+    isCrossTarget = super.stdenv.buildPlatform != super.stdenv.targetPlatform;
   in
   {
     ubootTools = (
@@ -34,20 +28,33 @@
       super.callPackage ../packages/linux-6.12-99pi.nix { dtbname = "ls2k300_99pi_wifi"; }
     );
 
+    ubootLs99PiTF =
+      (super.buildUBoot {
+        defconfig = "loongson_2k300_99pi_tf_defconfig";
+        extraMeta.platforms = [ "loongarch64-linux" ];
+        filesToInstall = [ "u-boot-with-spl.bin" ];
+        src = super.fetchgit {
+          url = "https://gitee.com/open-loongarch/u-boot.git";
+          rev = "96038f5a0c757fab5606ef916055c61f741aca07";
+          sha256 = "sha256-OpPOCVR5eel0U3wenBNaRmaMEv7WYbBiWZfexEfo+Dw=";
+        };
+        version = "2024.04";
+        extraConfig = ''
+          CONFIG_CMD_SYSBOOT=y
+        '';
+      }).overrideAttrs
+        ({ patches = [ ./initrd-support.patch ]; });
+
     rustPlatform_1_83 = super.makeRustPlatform {
       cargo = self.buildPackages.rust-bin.stable."1.83.0".minimal;
       rustc = self.buildPackages.rust-bin.stable."1.83.0".minimal;
     };
 
-    mercurial = super.mercurial.override {
-      cargo = self.buildPackages.rust-bin.stable."1.83.0".minimal;
-      rustc = self.buildPackages.rust-bin.stable."1.83.0".minimal;
-    };
+    rustPlatform = if isCross then self.rustPlatform_1_83 else super.rustPlatform;
 
-    fish = super.fish.override {
-      cargo = self.buildPackages.rust-bin.stable."1.83.0".minimal;
-      rustc = self.buildPackages.rust-bin.stable."1.83.0".minimal;
-    };
+    cargo = if isCrossTarget then self.buildPackages.rust-bin.stable."1.83.0".minimal else super.cargo;
+    rustc = if isCrossTarget then self.buildPackages.rust-bin.stable."1.83.0".minimal else super.rustc;
+
   }
-  // (super.lib.genAttrs rustPackages (name: updateRustPlatform super.${name}))
+
 )
