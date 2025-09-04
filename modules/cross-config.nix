9f406e75f098d7d8f9f053e3b1653f01b810dad9
fzdslr-nw <fzdslr@qq.com>
2025-09-04 14:00:06 +0000

    feat: follow upstream loongarch64-linux-embedded settings

--- modules/cross-config.nix
+++ modules/cross-config.nix
@@ -1,15 +1,15 @@
-{ lib, pkgs, rust-overlay, ... }:
 {
-  nixpkgs.crossSystem = {
-    system = "loongarch64-linux";
-    config = "loongarch64-unknown-linux-gnu";
-    gcc.arch = "loongarch64";
+  lib,
+  pkgs,
+  rust-overlay,
+  ...
+}:
+{
+  nixpkgs.crossSystem = lib.recursiveUpdate lib.systems.examples.loongarch64-linux-embedded {
     linux-kernel = {
       name = "loong64";
       target = "uImage";
     };
-#     rust.rustcTarget = "loongarch64-unknown-linux-gnu";
-#     rust.platform = builtins.fromJSON (builtins.readFile ./rust-loongarch64-gnu-nosimd.json);
   };
   nixpkgs.overlays = [
     (import ../overlays/default.nix)
